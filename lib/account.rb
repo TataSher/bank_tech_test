370924d68bb3fa8c70f88422374dbe0217808875
Natalia_Sherchenkova <tataterer@gmail.com>
2021-05-04 14:48:21 +0100

    Refactored to pass rubocop tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,12 @@
+# Account will allow users to make deposits/withdrawals and see statements
 class Account
-  def make_deposit(amount)
-    @amount = amount.to_f
-    "You made a deposit of £#{'%.2f' % @amount}"
+  def make_deposit(deposit)
+    @deposit = deposit
+    "Deposit: £#{format('%.2f', @deposit)}"
+  end
+
+  def make_withdrawal(withdrawal)
+    @withdrawal = withdrawal
+    "Withdrawal: £#{format('%.2f', @withdrawal)}"
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,6 +5,11 @@ require 'account'
 describe Account do
   it 'can make a deposit' do
     account = Account.new
-    expect(account.make_deposit(500.00)).to eq('You made a deposit of £500.00')
+    expect(account.make_deposit(500.00)).to eq('Deposit: £500.00')
+  end
+  it 'can make a withdrawal' do
+    account = Account.new
+    account.make_deposit(500.00)
+    expect(account.make_withdrawal(100.00)).to eq('Withdrawal: £100.00')
   end
 end
