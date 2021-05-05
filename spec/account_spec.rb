2f61d9916f66f7c7990f08dba263c3e3afcde182
Natalia_Sherchenkova <tataterer@gmail.com>
2021-05-05 12:37:21 +0100

    date test added for withdrawal feature

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,13 +4,13 @@ require 'account'
 require 'timecop'
 
 describe Account do
-    before do
-      Timecop.freeze(Time.new(2021, 05, 04))
-    end
-    after do
-      Timecop.return
-    end
-  
+  before do
+    Timecop.freeze(Time.new(2021, 05, 04))
+  end
+  after do
+    Timecop.return
+  end
+
   it 'can make a deposit' do
     account = Account.new
     expect(account.make_deposit(500.00)).to include('£500.00')
@@ -25,5 +25,11 @@ describe Account do
     account = Account.new
     account.make_deposit(500.00)
     expect(account.make_deposit(500)).to eq('Deposit 04/05/2021: £500.00')
+  end
+
+  it 'shows a date when withdrawal is made' do
+    account = Account.new
+    account.make_deposit(500.00)
+    expect(account.make_withdrawal(100)).to eq('Withdrawal 04/05/2021: £100.00')
   end
 end
